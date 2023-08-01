20f8005e1739fa9e70214379e0ae53820fa06b78
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2023-08-01 20:55:07 +0300

    design models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from taggit.managers import TaggableManager
 
-# Create your models here.
+class Question(models.Model):
+    author=models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True ,related_name='author_question')
+    question = models.CharField(max_length=350)
+    tags =TaggableManager()
+    created_at=models.DateTimeField(default=timezone.now)
+    content=models.CharField(max_length=30)
+
+class Answer(models.Model):
+        author=models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True,related_name='author_answer')
+        answer=models.TextField(max_length=2000)
+        question = models.ForeignKey(Question,on_delete=models.SET_NULL,null=True,blank=True,related_name='question_answer')
+        created_at=models.DateTimeField(default=timezone.now)
