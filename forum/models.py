ebe53880a4b20772da929cc701cbab96c484cd04
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2023-08-01 21:17:15 +0300

    small model adjustment

--- forum/models.py
+++ forum/models.py
@@ -4,14 +4,18 @@ from django.utils import timezone
 from taggit.managers import TaggableManager
 
 class Question(models.Model):
-    author=models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True ,related_name='author_question')
     question = models.CharField(max_length=350)
+    author=models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True ,related_name='author_question')
     tags =TaggableManager()
-    created_at=models.DateTimeField(default=timezone.now)
     content=models.CharField(max_length=30)
+    created_at=models.DateTimeField(default=timezone.now)
+    def __str__(self):
+         return self.question
 
 class Answer(models.Model):
-        author=models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True,related_name='author_answer')
         answer=models.TextField(max_length=2000)
         question = models.ForeignKey(Question,on_delete=models.SET_NULL,null=True,blank=True,related_name='question_answer')
+        author=models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True,related_name='author_answer')
         created_at=models.DateTimeField(default=timezone.now)
+        def __str__(self):
+            return self.question
